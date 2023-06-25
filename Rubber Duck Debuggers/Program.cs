0d72b5b934c3849406ec057db1ba0c6c40cb2e20
Stefan Sabev <stevenbg91@gmail.com>
2023-06-25 14:47:08 +0200

    Initial commit

---
+++ Rubber Duck Debuggers/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] time = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+int[] tasks = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+Queue<int> timeQueue = new Queue<int>(time);
+Stack<int> tasksStack = new Stack<int>(tasks);
+
+Dictionary<string, int> values = new Dictionary<string, int>();
+values.Add("Darth Vader Ducky", 0);
+values.Add("Thor Ducky", 0);
+values.Add("Big Blue Rubber Ducky", 0);
+values.Add("Small Yellow Rubber Ducky", 0);
+
+
+while (timeQueue.Count != 0 && tasksStack.Count != 0)
+{
+    int currentTime = timeQueue.Dequeue();
+    int currentTask = tasksStack.Pop();
+    int result = currentTime * currentTask;
+
+    if (result >= 0 && result <= 60)
+    {
+        values["Darth Vader Ducky"] += 1;
+    }
+    else if (result >= 61 && result <= 120)
+    {
+        values["Thor Ducky"] += 1;
+    }
+    else if (result >= 121 && result <= 180)
+    {
+        values["Big Blue Rubber Ducky"] += 1;
+    }
+    else if (result >= 181 && result <= 240)
+    {
+        values["Small Yellow Rubber Ducky"] += 1;
+    }
+    else if (result > 240)
+    {
+          tasksStack.Push(currentTask - 2);
+        timeQueue.Enqueue(currentTime);
+    }
+}
+Console.WriteLine("Congratulations, all tasks have been completed! Rubber ducks rewarded:");
+
+foreach(var item in values)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
---
+++ The Squirrel/Program.cs
@@ -0,0 +1,177 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] moves = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+Queue<string> queue = new Queue<string>(moves);
+char[,] matrix = new char[n, n];
+int playerRow = 0;
+int playerCol = 0;
+int hazelnuts = 0;
+for (int row = 0; row < n; row++)
+{
+    string input = Console.ReadLine();
+
+    for (int col = 0; col < n; col++)
+    {
+        matrix[row, col] = input[col];
+
+        if (matrix[row, col] == 's')
+        {
+            playerRow = row;
+            playerCol = col;
+        }
+
+    }
+}
+
+while (queue.Count != 0)
+{
+    string currentMove = queue.Dequeue();
+
+    if (currentMove == "left")
+    {
+        if (isValidPosition(matrix, playerRow, playerCol - 1))
+        {
+            if (matrix[playerRow, playerCol - 1] == 't')
+            {
+                Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+                Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+                Environment.Exit(0);
+            }
+            else if (matrix[playerRow, playerCol - 1] == 'h')
+            {
+                hazelnuts++;
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow, playerCol - 1] = 's';
+            }
+            else if (matrix[playerRow, playerCol - 1] == '*')
+            {
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow, playerCol - 1] = 's';
+            }
+            playerCol--;
+
+
+        }
+        else
+        {
+            Console.WriteLine("The squirrel is out of the field.");
+            Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+            Environment.Exit(0);
+
+        }
+    }
+    else if (currentMove == "right")
+    {
+        if (isValidPosition(matrix, playerRow, playerCol + 1))
+        {
+            if (matrix[playerRow, playerCol + 1] == 't')
+            {
+                Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+                Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+                Environment.Exit(0);
+            }
+            else if (matrix[playerRow, playerCol + 1] == 'h')
+            {
+                hazelnuts++;
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow, playerCol + 1] = 's';
+            }
+            else if (matrix[playerRow, playerCol - 1] == '*')
+            {
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow, playerCol + 1] = 's';
+            }
+            playerCol++;
+
+        }
+        else
+        {
+            Console.WriteLine("The squirrel is out of the field.");
+            Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+            Environment.Exit(0);
+
+        }
+    }
+    else if (currentMove == "up")
+    {
+        if (isValidPosition(matrix, playerRow - 1, playerCol))
+        {
+            if (matrix[playerRow - 1, playerCol] == 't')
+            {
+                Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+                Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+                Environment.Exit(0);
+            }
+            else if (matrix[playerRow - 1, playerCol] == 'h')
+            {
+                hazelnuts++;
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow - 1, playerCol] = 's';
+            }
+            else if (matrix[playerRow, playerCol - 1] == '*')
+            {
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow - 1, playerCol] = 's';
+            }
+           
+            playerRow--;
+        }
+        else
+        {
+            Console.WriteLine("The squirrel is out of the field.");
+            Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+            Environment.Exit(0);
+
+        }
+    }
+    else if (currentMove == "down")
+    {
+        if (isValidPosition(matrix, playerRow + 1, playerCol))
+        {
+            if (matrix[playerRow + 1, playerCol] == 't')
+            {
+                Console.WriteLine("Unfortunately, the squirrel stepped on a trap...");
+                Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+                Environment.Exit(0);
+            }
+            else if (matrix[playerRow + 1, playerCol] == 'h')
+            {
+                hazelnuts++;
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow + 1, playerCol] = 's';
+            }
+            else if (matrix[playerRow, playerCol - 1] == '*')
+            {
+                matrix[playerRow, playerCol] = '*';
+                matrix[playerRow + 1, playerCol] = 's';
+            }
+           
+            playerRow++;
+        }
+        else
+        {
+            Console.WriteLine("The squirrel is out of the field.");
+            Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+            Environment.Exit(0);
+
+        }
+    }
+
+    if (hazelnuts == 3)
+    {
+        Console.WriteLine("Good job! You have collected all hazelnuts!");
+        Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+        Environment.Exit(0);
+    }
+}
+
+Console.WriteLine("There are more hazelnuts to collect.");
+Console.WriteLine($"Hazelnuts collected: {hazelnuts}");
+
+bool isValidPosition(char[,] matrix, int playerRow, int playerCol)
+
+{
+    return playerRow >= 0 && playerRow < matrix.GetLength(0) && playerCol >= 0 && playerCol < matrix.GetLength(1);
+
+
+}
